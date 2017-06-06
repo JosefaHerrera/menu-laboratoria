d2904ff7aaf4ba7d1f9a17128c723b68b1a0e51f
JosefaHerrera <josefa@linuxero.cl>
2017-06-06 14:35:50 -0400

    afinando detalles

---
+++ js/main.js
@@ -0,0 +1,68 @@
+var hamburguesa = document.getElementById("hamburguesa");
+
+
+hamburguesa.addEventListener("click",function(){
+	var lista2= document.getElementsByClassName("lista2")[0];
+
+	var menu1 = document.createElement("div");
+		menu1.setAttribute("class","general");
+	var parrafo = document.createElement("p");
+		parrafo.setAttribute("class","texto");
+
+
+	var menu2 = document.createElement("div");
+		menu2.setAttribute("class","general");
+	var parrafo2 = document.createElement("p");
+		parrafo2.setAttribute("class","texto");
+
+	var menu3 = document.createElement("div");
+		menu3.setAttribute("class","general");
+	var parrafo3 = document.createElement("p");
+		parrafo3.setAttribute("class","texto");
+
+	var menu4 = document.createElement("div");
+		menu4.setAttribute("class","general");
+	var parrafo4 = document.createElement("p");
+		parrafo4.setAttribute("class","texto");
+
+	var menu5 = document.createElement("div");
+		menu5.setAttribute("class","general");
+	var parrafo5 = document.createElement("p");
+		parrafo5.setAttribute("class","texto");
+
+
+	var home = document.createTextNode("Home");
+	var about = document.createTextNode("About");
+	var products = document.createTextNode("Products");
+	var services =document.createTextNode("Services");
+	var costumers = document.createTextNode("Costumers");
+	var contact = document.createTextNode("Contact");
+
+	/*HOME*/
+	parrafo.appendChild(home);
+	menu1.appendChild(parrafo);
+	lista2.appendChild(menu1);
+	/*ABOUT*/
+	parrafo2.appendChild(about);
+	menu2.appendChild(parrafo2);
+	lista2.appendChild(menu2);
+	/*SERVICES*/
+	parrafo3.appendChild(services);
+	menu3.appendChild(parrafo3);
+	lista2.appendChild(menu3);
+	/*COSTUMERS*/
+	parrafo4.appendChild(costumers);
+	menu4.appendChild(parrafo4);
+	lista2.appendChild(menu4);
+	/*CONTACT*/
+	parrafo5.appendChild(contact);
+	menu5.appendChild(parrafo5);
+	lista2.appendChild(menu5);
+
+
+
+
+
+
+});
+
